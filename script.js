d04c4aa71edb7a329e7482e11edd758dfcf7e2a5
Rachael <124227903+Lakaeli@users.noreply.github.com>
2023-03-24 18:03:33 -0500

    pushed final commit on js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// Assignment code here
 
-// Get references to the #generate element
+// create var library
 var generateBtn = document.querySelector("#generate");
 var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
 var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numericNumbers = "0123456789"
 var specialCharacters = "!~`!@#$%^&*()-_+={}[]|\;:./?,"
 var characterPool = ""
-// Write password to the #password input
+var password = ""
+// write password function
 function writePassword() {
+  // write password length pompt to set variable
   var passwordLength = prompt ("How long would you like your password to be? (Between 8-128 characters"); 
 
-  
   if (passwordLength < 8 || passwordLength > 128) {
     console.log ("Invalid password length");
     alert("Password does not meet requirements")
     return 
   }
-
+// write password variables to include via homework requirements
   var useLowercase = confirm ("Would you like to include lowercase letters?")
   console.log (useLowercase)
 
@@ -39,26 +39,31 @@ if (useNumeric === true){
   characterPool = characterPool + numericNumbers
 }
  
-var useSpecialCharacters = confirm ("You must use one special character.")
+var useSpecialCharacters = confirm ("Would you like to use special characters?")
  console.log (specialCharacters)
 
  if (useSpecialCharacters === true){
   characterPool = characterPool + specialCharacters
  }
+// write if/return statement
+if (!useSpecialCharacters&& !useNumeric && !useUppercase && !useLowercase) {
+  alert ("You must choose at least one option")
+  return
+}
+// write for loop using math.floor/math.random methods
+for (var i = 0; i < passwordLength; i++) {
+var randomNumber = Math.floor(Math.random() * (characterPool.length - 0) + 0);
 
-
-
-  // var password = generatePassword();
+// write var password random selection 
+ password = characterPool.charAt(randomNumber) + password
+ console.log (password)
+}
+  
   var passwordText = document.querySelector("#password");
  
- 
-// Math.random () and Math.floor methods?
-
-// document.getElementById("password");
   passwordText.value = password;
-
 }
-// Add event listener to generate button
+// add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//display password via an alert
+
